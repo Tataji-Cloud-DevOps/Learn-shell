58757e4bb92cc3c78e62d3c68a3618953882536d
Tataji2426 <you@example.com>
2025-06-12 22:25:42 +0530

    Auto Push

--- 004-variables.sh
+++ 004-variables.sh
@@ -7,3 +7,13 @@
 # a=10
 # name=mike 
 # op=plus 
+
+# In java,  int a=10 , char a=X , string x=Mike, float a=1.5
+
+# the advantage & disadvantage in shell is that there are no Data Types
+
+# In bash, everything will be considered as string by default.
+
+a=10                                # ( a is key and 10 is the value )
+b=1.5                               # Hardcoding the values 
+name=mike 

1f47967797e7839d70ea824c47c9ebe10cba2118
Tataji2426 <you@example.com>
2025-06-16 23:39:59 +0530

    Auto Push

--- 005-dynamicVars.sh
+++ 005-dynamicVars.sh
@@ -15,4 +15,10 @@ echo "Todays date is $DATE"
 # echo "Script Executed By $EXEC"
 echo "Script Executed by $EXEC and from $DEPARTMENT"
 
+
 # Arguments  : These arguments help sin fetching the values from the command line 
+# These are mostly used to allow the users to supply the value or to override the values
+
+# bash scriptName.sh   arg1  arg2 arg3 .. . . . arg9 
+# bash scriptName.sh   firstArg secondArg  thrigArg 
+# bash scriptName  10 20 30 

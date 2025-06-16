4682ae8ad5fddb4003b92caa2e6863984b7331fb
Tataji2426 <you@example.com>
2025-06-16 23:26:33 +0530

    Auto Push

--- 005-dynamicVars.sh
+++ 005-dynamicVars.sh
@@ -3,3 +3,5 @@
 # DATE="03AUG2024"                # This is called hardCoding 
  
 DATE=$(date +%F)                  # This is how we can dynamically fetch the values (This paranthesis is mainly used for expression)
+
+echo "Todays date is $DATE"

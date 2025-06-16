69f8656e532310ab8551446349b1dc159e5ab79f
Tataji2426 <you@example.com>
2025-06-16 23:39:23 +0530

    Auto Push

--- 005-dynamicVars.sh
+++ 005-dynamicVars.sh
@@ -5,3 +5,14 @@
 DATE=$(date +%F)                  # This is how we can dynamically fetch the values (This paranthesis is mainly used for expression)
 
 echo "Todays date is $DATE"
+
+# Arguments 
+
+EXEC=$1 
+DEPARTMENT=$2
+echo "Todays date is $DATE"
+
+# echo "Script Executed By $EXEC"
+echo "Script Executed by $EXEC and from $DEPARTMENT"
+
+# Arguments  : These arguments help sin fetching the values from the command line 

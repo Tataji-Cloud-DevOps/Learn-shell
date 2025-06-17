404572f413e121d782de6a80d968be83d7f666ba
Tataji2426 <you@example.com>
2025-06-17 22:41:47 +0530

    Auto Push

--- 009-read.sh
+++ 009-read.sh
@@ -4,3 +4,5 @@ echo "This script will proceed by taking input from the user"
 read -p "Enter Value Of A:" A
 read -p "Enter Value Of B:" B
 echo "Value of A is $A" 
+echo "Value of B is $B" 
+echo "Sum of A + B  = $((A+B))"   # Expresisons that are arithmetic should be enclosed in double paranthesis

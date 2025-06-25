1cff485044c3cdb7d69914f4e41e60218d44c039
Tataji2426 <you@example.com>
2025-06-25 21:29:54 +0530

    Auto Push

--- 011-redirectors.sh
+++ 011-redirectors.sh
@@ -8,6 +8,19 @@
 #     2) Output Redirector  ( >> or > )( Means routing the output to a file )        :    > or 1>  or >>  or  &>  ( >> appends the latest output to the existing content)  
 
     # Outputs 
-        1) Standard Output                          1> or > or >> ( Append )
-        2) Standard Error                           2> or 2>> 
-        3) Standard Output and Standard Error       &> or &>> 
+        1) Standard Output                          1> or > or >> ( Append ) # This output will come by without error
+        2) Standard Error                           2> or 2>>                # This will come with both output and error
+        3) Standard Output and Standard Error       &> or &>>
+
+         
+# ls -ltr    >  output.txt   # Redirects the output to  output.txt
+# ls -ltr    >> output.txt   # Redirects and appends the output to  output.txt
+# ls -ltr    2> output.txt   # Redirects the error only to  output.txt
+# ls -ltr    &> output.txt   # Redirects the output or error output.txt
+
+
+
+## How inputs and outputs are categorized :
+
+#     1) Standard Output   ( Expected error less output )
+#     2) Standard Error    ( Errored output   )

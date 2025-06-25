3e1d71e58ee733828490454e9d871269c7587c1c
Tataji2426 <you@example.com>
2025-06-26 05:29:18 +0530

    Auto Push

---
+++ 012-conditions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash  
+
+
+# If the value is this, then do that.
+# If the value is this,then do that, if not do this
+
+# If or ifelse of else if conditions. 
+
+#!/bin/bash
+
+<<COMMENT
+if condition is ideally available in 3 formats :
+    1) Simple If 
+    2) If Else 
+    3) Else If  
+
+# syntax
+
+1) Simple If :  
+
+    if [ expression ]; then
+            commands
+    fi 
+
+    Commands are going to be executed only if the expression is true. 
+
+* What will happen if the expression  or false  ? Simple, commands won't be executed.

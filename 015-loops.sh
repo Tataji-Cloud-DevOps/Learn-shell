81347a0030be585dd1cf4b76c33481eaba98e62e
Tataji2426 <you@example.com>
2025-06-26 06:23:22 +0530

    Auto Push

--- 015-loops.sh
+++ 015-loops.sh
@@ -7,6 +7,20 @@
 #     2) While Loop      ( when you don't know how many time to be executed, we use this and while only runs if the condition is trye )
 
 
+
+# for loop syntax
+
+#syntax example 1
+for var in a b c d ; do   
+    echo "Value is $var" 
+done 
+
+#syntax example 2
 for var in tataji pandu sai; do
+    echo "Value is $var" 
+done 
+
+#syntax example 3
+for var in 1 2 3 4 ; do
     echo "Value is $var" 
 done 

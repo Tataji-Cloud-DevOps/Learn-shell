7c6f0cd6e42a302b2ea56e7ae2c7dc16f965244a
Tataji2426 <you@example.com>
2025-06-26 05:52:19 +0530

    Auto Push

--- 013-if.sh
+++ 013-if.sh
@@ -11,11 +11,13 @@ ACTION=$1
 
 
 
-# Demo on if else 
-# if [ "$ACTION" = "start" ] ; then 
-#     echo -e "\e[32m Backed is starting \e[0m"
-#     exit 0
-# else 
-#     echo -e "\e[31m start is valid option \e[0m"
-#     exit 3
-# fi  
+ Demo on if else 
+if [ "$ACTION" = "start" ] ; then 
+     echo -e "\e[32m Backed is starting \e[0m"
+     exit 0
+ else 
+     echo -e "\e[31m start is valid option \e[0m"
+     exit 3
+ fi  
+
+ 

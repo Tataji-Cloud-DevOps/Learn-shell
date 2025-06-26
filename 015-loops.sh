aa0c05daa07a3e96ee0e5a74b71719e0fa9eb5a6
Tataji2426 <you@example.com>
2025-06-26 06:27:42 +0530

    Auto Push

--- 015-loops.sh
+++ 015-loops.sh
@@ -8,19 +8,19 @@
 
 
 
-# for loop syntax
+# for loop syntax                            # it will print  valu of a, value of b, value of c, value of d
 
 #syntax example 1
 for var in a b c d ; do   
     echo "Value is $var" 
 done 
 
-#syntax example 2
+#syntax example 2                            # it will print  value of tataji, value of pandu , value of sai
 for var in tataji pandu sai; do
     echo "Value is $var" 
 done 
 
-#syntax example 3
+#syntax example 3                            # it will print  valu of 1, value of 2, value of 3, value of 4
 for var in 1 2 3 4 ; do
     echo "Value is $var" 
 done 

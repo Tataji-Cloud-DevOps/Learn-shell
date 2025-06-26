31cd8d3a08dcd37b9326285876491b8ee47c8a47
Tataji2426 <you@example.com>
2025-06-26 05:48:37 +0530

    Auto Push

--- 013-if.sh
+++ 013-if.sh
@@ -1,10 +1,21 @@
 #!/bin/bash 
 
-echo -e "Demo On If condition" 
+echo -e "Demo On If condition"                           #If the condition is use the if
 
 ACTION=$1
 
- if [ "$ACTION" = "start" ] ; then 
-     echo -e "\e[32m Backed is starting \e[0m"
-    exit 0
- fi  
+# if [ "$ACTION" = "start" ] ; then 
+#     echo -e "\e[32m Backed is starting \e[0m"
+#     exit 0
+# fi  
+
+
+
+# Demo on if else 
+# if [ "$ACTION" = "start" ] ; then 
+#     echo -e "\e[32m Backed is starting \e[0m"
+#     exit 0
+# else 
+#     echo -e "\e[31m start is valid option \e[0m"
+#     exit 3
+# fi  
